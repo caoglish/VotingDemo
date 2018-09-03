0038c68ac696c5aa34cfe5d8de673935e6fd01eb
caoglish <caoglish.email@gmail.com>
2018-09-03 16:52:30 +1000

    clean up code

--- index.js
+++ index.js
@@ -1,64 +1,61 @@
+/*eslint no-console: ["error", { allow: ["warn", "log"] }] */
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(3000);
-
+//route
 app.get('/', function (request, response) {
-	// response.send('Hello,wolrd');
-	response.sendFile(__dirname + "/index.html");
+	response.sendFile(__dirname + '/index.html');
 });
 
 
 app.get('/static', function (request, response) {
-	// response.send('Hello,wolrd');
-	response.sendFile(__dirname + "/staticCenter.html");
+	response.sendFile(__dirname + '/staticCenter.html');
 });
 
- //var voting = [];
-//var clientList=[];
+
 var clientDataList=[];
 
-var static = io.of("/static").on('connect', (socket) => {
+//staticCenter
+var staticCenter = io.of('/static').on('connect', (socket) => {
 	console.log('static connect have been made.');
 	io.emit('connected', 'static connect have been made.');
-	//static.emit("static.selection", voting);
-	static.emit("static.clientList",clientDataList);
-	console.log(clientDataList);
+	staticCenter.emit('static.clientList',clientDataList);
 });
 
 
-
-var client = io.of("/client").on('connect', (socket) => {
+//client
+var client = io.of('/client').on('connect', (socket) => {
+	console.log('client['+socket.id + '] connect have been made.');
 	
-	console.log("client["+socket.id + '] connect have been made.');
-    let clientData = {
+	let clientData = {
 		id: socket.id,
 		username:null,
 		selection:null
-    };
+	};
 	clientDataList.push(clientData);
 	
-	socket.emit("user.name", socket.id);
-	static.emit("static.clientList", clientDataList);
+	socket.emit('user.name', socket.id);
+	staticCenter.emit('static.clientList', clientDataList);
 	
 	
 	socket.on('vote', function (selection) {
 		clientData.selection = selection;
-		static.emit("static.clientList", clientDataList);
+		staticCenter.emit('static.clientList', clientDataList);
 	});
 
 	socket.on('updateName', function (username) {
 		clientData.username = username;
-		static.emit("static.clientList", clientDataList);
+		staticCenter.emit('static.clientList', clientDataList);
 	});
 
 	socket.on('disconnect', function () {
-		console.log("client[" + socket.id + '] is disconnected.');
+		console.log('client[' + socket.id + '] is disconnected.');
 		clientDataList=clientDataList.filter((v) => {
-			return v.id != socket.id
+			return v.id != socket.id;
 		});
-		static.emit("static.clientList", clientDataList);
+		staticCenter.emit('static.clientList', clientDataList);
 	});
 
 });
